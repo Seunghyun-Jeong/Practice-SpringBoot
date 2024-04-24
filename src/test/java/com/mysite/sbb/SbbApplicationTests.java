2771a3463fb5ff3a2f5b1fe9344b1952d525b897
Seunghyun-Jeong <dkskakdgka@gmail.com>
2024-04-24 16:23:56 +0900

    test: findBySubject 메서드 추가 및 이전 테스트 원복

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -17,10 +17,19 @@ class SbbApplicationTests {
 
 	@Test
 	void testJpa() {
+		List<Question> all = this.questionRepository.findAll();
+		assertEquals(2, all.size());
+
+		Question q1 = all.get(0);
+		assertEquals("sbb가 무엇인가요?", q1.getSubject());
+
 		Optional<Question> oq = this.questionRepository.findById(1);
 		if(oq.isPresent()) {
-			Question q = oq.get();
-			assertEquals("sbb가 무엇인가요?", q.getSubject());
+			Question q2 = oq.get();
+			assertEquals("sbb가 무엇인가요?", q2.getSubject());
 		}
+
+		Question q3 = this.questionRepository.findBySubject("sbb가 무엇인가요?");
+		assertEquals(1, q3.getId());
 	}
 }
