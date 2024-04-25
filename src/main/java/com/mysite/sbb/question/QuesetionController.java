cc47dd1affb0bd941ad23971963c7516cec1697a
Seunghyun-Jeong <dkskakdgka@gmail.com>
2024-04-25 16:54:18 +0900

    feat: 템플릿 설정

--- src/main/java/com/mysite/sbb/question/QuesetionController.java
+++ src/main/java/com/mysite/sbb/question/QuesetionController.java
@@ -2,13 +2,11 @@ package com.mysite.sbb.question;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class QuesetionController {
     @GetMapping("/question/list")
-    @ResponseBody
     public String List() {
-        return "question list";
+        return "question_list";
     }
 }
