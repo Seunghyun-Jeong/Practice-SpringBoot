bd22b42421207cb183525f43fed30ae95766551b
Seunghyun-Jeong <dkskakdgka@gmail.com>
2024-04-25 17:49:35 +0900

    feat: 데이터를 템플릿에 전달

--- src/main/java/com/mysite/sbb/question/QuesetionController.java
+++ src/main/java/com/mysite/sbb/question/QuesetionController.java
@@ -1,12 +1,22 @@
 package com.mysite.sbb.question;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Controller
 public class QuesetionController {
+
+    private final QuestionRepository questionRepository;
+
     @GetMapping("/question/list")
-    public String List() {
+    public String List(Model model) {
+        List<Question> questionsList = this.questionRepository.findAll();
+        model.addAttribute("questionsList", questionsList);
         return "question_list";
     }
 }
